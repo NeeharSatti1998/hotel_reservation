5e28c9c7a7c2f19c77f1844b053dfc00b4f23f02
NeeharSatti1998 <sattineehar@gmail.com>
2025-07-09 01:34:49 -0400

    syntax correction

--- config/model_params.py
+++ config/model_params.py
@@ -2,14 +2,14 @@ from scipy.stats import randint,uniform
 
 
 LIGHTGBM_PARAMS = {
-    'num_leaves' : randint(20,150),
-    'max_depth' : randint(3,10),
-    'learning_rate' : uniform(0.01,0.3),
-    'n_estimators' : randint(100,1000),
-    'min_child_sample' : randint(10,50),
-    'subsample' : uniform(0.6,0.4),
-    'colsample_tree' : uniform(0.5,0.5)
-}
+    'num_leaves': randint(20, 150),
+    'max_depth': randint(3, 10),
+    'learning_rate': uniform(0.01, 0.3),
+    'n_estimators': randint(100, 1000),
+    'min_child_samples': randint(10, 50),        
+    'subsample': uniform(0.6, 0.4),                   
+    'colsample_bytree': uniform(0.5, 0.5)
+}         
 
 
 RANDOM_SEARCH_PARAMS = {
